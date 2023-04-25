4d2c4506866bfaa3d7fcd793da24ed6b1eed8b99
dymovv1 <romandymovv1997@gmail.com>
2023-04-25 14:01:29 +0200

    1

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Створити HTML-сторінку з кнопкою "Відкрити" і модальним вікном. На модальном вікні повинен бути текст і кнопка "Закрити". 
+// Спочатку модальне вікно не відображається. 
+// При кліку на кнопку "Відкрити" з'являється модальне вікно, на кнопку "Закрити" — зникає.
+
+
 const openButton = document.getElementById('open');
 const closedButton = document.getElementById('closed');
 const modalWindow = document.querySelector('.modal');
@@ -14,5 +19,36 @@ closedButton.addEventListener('click', function() {
   modalWindow.classList.remove('open-window');
 });
 
+
+// Створити HTML-сторінку зі світлофором і кнопкою, яка перемикає світлофор на наступний колір.
+
+const buttonTurn = document.querySelector('.btn-turn');
+const lightGreen = document.querySelector('.green');
+const lightYellow = document.querySelector('.yellow');
+const lightRed = document.querySelector('.red');
+
+let currentColor = lightGreen;
+
+buttonTurn.addEventListener('click', function() {
+  switch (currentColor) {
+    case lightGreen:
+      currentColor = lightYellow;
+      lightGreen.style.opacity = '0.2';
+      lightYellow.style.opacity = '1';
+      break;
+    case lightYellow:
+      currentColor = lightRed;
+      lightYellow.style.opacity = '0.2';
+      lightRed.style.opacity = '1';
+      break;
+    case lightRed:
+      currentColor = lightGreen;
+      lightRed.style.opacity = '0.2';
+      lightGreen.style.opacity = '1';
+      break;
+    default:
+      break;
+  }
+});
 
 
